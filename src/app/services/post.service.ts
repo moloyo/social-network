c34e8b64264f014d2b9b3e3da2e300cfbe51a523
Joaquín García <isi.garciaj@gmail.com>
2018-10-18 13:42:57 -0300

    Fix Service

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from './api.service';
 import { Post } from '../models/post';
-import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
